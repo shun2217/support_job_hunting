cba4c7d63a4bc2ceeda730b31abe420592a604a0
shun2217 <next0710@docomo.ne.jp>
2025-03-29 17:27:08 +0000

    ベースの作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # ページごとの完全なタイトルを返す
+  def full_title(page_title = '')
+    base_title = "就活情報管理"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
