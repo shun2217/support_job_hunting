94cc8bafbec8d1398f145e533433fd6ff1b1609a
shun2217 <next0710@docomo.ne.jp>
2025-04-05 17:05:28 +0000

    ベースの作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,13 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
+
+  def sidebar_link_item(name, path)
+    class_name = 'channel'
+    class_name << ' active' if current_page?(path)
+
+    content_tag :li, class:class_name do
+      link_to name, path, class: 'channel_name'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'base#home'
+
+  resources :companies # 会社一覧
 end
