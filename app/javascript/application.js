f4b2b1a154a110f636fb996d8455430b59331f4f
shun2217 <next0710@docomo.ne.jp>
2025-04-21 17:05:13 +0000

    i18nの設定

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -12,6 +12,28 @@ document.addEventListener("turbo:load", () => {
   if (table) {
     $.getJSON("/companies.json", function(response) {
       $(table).DataTable({
+        "language": {
+          "sEmptyTable":     "データがありません",
+          "sInfo":           "_START_ 〜 _END_ 件／全 _TOTAL_ 件",
+          "sInfoEmpty":      "0 〜 0 件／全 0 件",
+          "sInfoFiltered":   "(全 _MAX_ 件よりフィルタリング)",
+          "sInfoPostFix":    "",
+          "sLengthMenu":     "_MENU_ 件表示",
+          "sLoadingRecords": "読み込み中...",
+          "sProcessing":     "処理中...",
+          "sSearch":         "検索:",
+          "sZeroRecords":    "一致するレコードがありません",
+          "oPaginate": {
+            "sFirst":    "先頭",
+            "sPrevious": "前",
+            "sNext":     "次",
+            "sLast":     "最終"
+          },
+          "oAria": {
+            "sSortAscending":  ": 昇順に並べ替え",
+            "sSortDescending": ": 降順に並べ替え"
+          },
+        },
         columns: response.columns.map(col => ({ title: col })),
         data: response.data
       });
